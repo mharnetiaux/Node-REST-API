09f2108a4cec2292c76b46a7b025375f5aae9f19
Matt Harnetiaux <mattskyh@gmail.com>
2017-07-07 16:05:30 -0700

    minor update.

--- src/routes/item.js
+++ src/routes/item.js
@@ -5,21 +5,18 @@ function getItems(req, res) {
     let query = Item.find({});
     query.exec((err, items) => {
         if(err) res.send(err);
-        //If no errors, send them back to the client
         res.json(items);
     });
 }
 
 
 function postItems(req, res) {
-    //Creates a new book
     let newItem = new Item(req.body);
-    //Save it into the DB.
     newItem.save((err,item) => {
         if(err) {
             res.send(err);
         }
-        else { //If no errors, send it back to the client
+        else {
             res.json({message: "Item successfully added!", item });
         }
     });
